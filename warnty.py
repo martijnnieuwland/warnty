8420a7e950e5956967fd43a9114072ac9e7253ea
Martijn Nieuwland <cloudsurfer.online@gmail.com>
2024-02-21 19:47:49 +0100

    first commit warnty

--- warnty.py
+++ warnty.py
@@ -1,18 +1,35 @@
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton
 import sys
-from PyQt5.QtWidgets import *
 
 
-class DlgMain(QDialog):
+
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("My GUI")
+
+        self.setWindowTitle("warnty")
+        self.setMinimumSize(400, 300)
+        self.setMaximumSize(1200, 800)
+        button = QPushButton("Press me!")
+
+        self.setCentralWidget(button)
 
 
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    dlgMain = DlgMain()
-    dlgMain.show()
-    sys.exit(app.exec_())
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec_()
+# class DlgMain(QDialog):
+#     def __init__(self):
+#         super().__init__()
+#         self.setWindowTitle("My GUI")
+
+
+# if __name__ == "__main__":
+#     app = QApplication(sys.argv)
+#     dlgMain = DlgMain()
+#     dlgMain.show()
+#     sys.exit(app.exec_())
 
 
 # One window to enter new warranty information
